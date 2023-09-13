ce766ef0e9ebf961ff2940ca5f545efaeb75fb8d
dokhanh107 <ngockhanh107a@gmail.com>
2023-09-13 17:03:12 +0700

    Microservices kafka

---
+++ base-domain/src/main/java/com/example/basedomain/dto/Order.java
@@ -0,0 +1,19 @@
+package com.example.basedomain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+    private String orderID;
+    private String name;
+    private int qty;
+    private double price;
+
+}
